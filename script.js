8bcba0b71bfe1c85621984f59fc9ae6bca84bc12
15797vishal <115143001+15797vishal@users.noreply.github.com>
2023-06-25 17:27:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+ function click() {
+ 	 var confirms=confirm("do you want to proceed ?");
+	 if(confirms){
+	console.log("You clicked OK. Proceeding...");
+	 }
+	 else{
+		console.log("You clicked Cancel. Exiting...");
+	 }
+ }
